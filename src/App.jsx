8f7e8f9b8414183bdf390117a00e211c46c50bb4
arianoorqorbani <aria.noorghorbani@gmail.com>
2024-01-22 16:40:57 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import NewPost from "./components/NewPost";
 import PostList from "./components/PostList";
 
 function App() {
   return (
     <>
+      <NewPost />
       <PostList />
     </>
   );
